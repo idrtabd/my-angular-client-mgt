4b08bf17e8750981ba6c7681ece20d2407797f00
Idrise Abdi <idriseabdi@mac.lan>
2025-04-28 12:05:11 -0500

    remove old web

--- web-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-// /src/main.ts
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [...appConfig.providers],
-})
-  .catch((err) => console.error(err));
