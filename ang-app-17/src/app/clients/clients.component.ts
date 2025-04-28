44ee10be851c55a85fa80afdb1c1a631f0cb90c7
Idrise Abdi <idriseabdi@mac.lan>
2025-04-27 21:53:55 -0500

    angular clients page simple working

--- ang-app-17/src/app/clients/clients.component.ts
+++ ang-app-17/src/app/clients/clients.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+// import common modules
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './clients.component.html',
-  styleUrl: './clients.component.scss'
+  styleUrls: ['./clients.component.scss']
 })
+
 export class ClientsComponent {
+  loading = true;
+
+  // ngInit function to set timeout and spinner for loading, simulates a loading delay
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
+
+  clients = [
+    { name: 'Client One' },
+    { name: 'Client Two' },
+    { name: 'Client Three' }
+  ];
+
+
+  // function to toggleDarkMode
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
+
 
 }
--- web-app/src/app/clients/clients.component.ts
+++ web-app/src/app/clients/clients.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   standalone: true,
   selector: 'app-clients',
-  imports: [CommonModule, HttpClientModule],   // <-- VERY IMPORTANT: import HttpClientModule
- templateUrl: './clients.component.html', 
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatListModule
+  ],
+  templateUrl: './clients.component.html'
 })
 export class ClientsComponent implements OnInit {
   clients: any[] = [];
